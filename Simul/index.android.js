457e07d4404c872973a33861d14e3623ec3cea66
hypatiah <danielle.hassid@gmail.com>
2016-09-17 15:48:36 -0700

    Create initial template for navigator

--- Simul/index.android.js
+++ Simul/index.android.js
@@ -21,7 +21,25 @@ class Simul extends Component {
       <Navigator
         initialRoute={{ title: 'Enter', index: 0 }}
         renderScene={(route, navigator) => {
-          return <Enter title={route.title} />
+          return (
+            <Enter
+             title={route.title}
+
+             onForward={ () => {
+               const nextIndex = route.index + 1;
+               navigator.push({
+                 title: 'Scene ' + nextIndex,
+                 index: nextIndex,
+               });
+             }}
+
+             onBack={() => {
+               if (route.index > 0) {
+                 navigator.pop();
+               }
+             }}
+           />
+          )
         }}
       />
     );
